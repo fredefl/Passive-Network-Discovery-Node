8c3e6aa229b7d59548171372d361fbdaed9c96ad
Frederik Lassen <frederik@ubuntu.(none)>
2013-03-25 07:31:32 -0700

    Working on Netbios parsing

--- main.js
+++ main.js
@@ -39,7 +39,8 @@ console.log("- Connecting to MySQL database...");
 mysqlConnection.connect();
 console.log("- Connected to MySQL database!");
 
-capture = pcap.createSession(program.interface, 'ether proto not 0x888e and ether proto not 0x88b7 and ether proto not 0xcccc');
+//capture = pcap.createSession(program.interface, 'ether proto not 0x888e and ether proto not 0x88b7 and ether proto not 0xcccc');
+capture = pcap.createSession(program.interface, 'port 137');
 
 if (program.clean) {
 	console.log("- Cleaning...");
@@ -51,13 +52,18 @@ console.log("- Started...");
 var startTime = new Date();
 
 capture.on('packet', function (raw_packet) {
-	var packet = pcap.decode.packet(raw_packet);
+	var packet;
+	try {
+		packet = pcap.decode.packet(raw_packet);
+	} catch (ex) {
+		console.log(ex);
+	}
 	var sourceMac = "00:00:00:00:00:00";
 	var destinationMac = "00:00:00:00:00:00";
 	var sourceIp = "0.0.0.0";
 	var destinationIp = "0.0.0.0";
 	var method = "none";
-
+	var netbiosName = null;
 	try {
 		if (typeof packet.link.arp != 'undefined') {
 			sourceMac = packet.link.arp.sender_ha;
@@ -86,11 +92,31 @@ capture.on('packet', function (raw_packet) {
 			}
 		}
 	} catch (error) {
+		console.log(error);
 		console.log(util.inspect(packet));
+	}
+
+	try {
+		if (typeof packet.link.ip.udp != 'undefined' && packet.link.ip.udp.dport == 137 && packet.link.ip.udp.length == 76) {
+			// From [13] to [12+32]
+			var netbiosName = "";
+			for (var i = 13; i <= 13 + 31; i += 2) {
+				var character = "";
+				var characterHex = packet.link.ip.udp.data[i].toString(16) + packet.link.ip.udp.data[i+1].toString(16);
+				if (characterHex in characterArray)
+					character = characterArray[characterHex];
+				netbiosName += character;
+			}
+			netbiosName = netbiosName.replace(/^(\s*)((\S+\s*?)*)(\s*)$/,"$2");
+		}
+	} catch (ex) {
+		console.log("Error in Netbios parsing", ex);
 	}
 
 	processInformation(sourceMac, sourceIp, method);
 	processInformation(destinationMac, destinationIp, method);
+	if (netbiosName !== null) 
+		processNetbiosName((sourceMac, sourceIp, method, netbiosName);
 });
 
 function processInformation (macAddress, ipAddress, method) {
@@ -107,8 +133,8 @@ function processInformation (macAddress, ipAddress, method) {
 					console.log(getTime() + "New host discovered: " + method + "\t" + macAddress + "\t" + ipAddress);
 				}
 			});
-			mysqlConnection.query('INSERT INTO ' + mysqlTable + ' VALUES(?, ?) ON DUPLICATE KEY UPDATE ip=?, mac=?',
-				[ipAddress ,macAddress, ipAddress, macAddress]);
+			mysqlConnection.query('INSERT INTO ' + mysqlTable + ' (ip, mac, method) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE ip=?, mac=?, method=?',
+				[ipAddress, macAddress, method, ipAddress, macAddress, method]);
 		}
 	}
 }
@@ -116,3 +142,5 @@ function processInformation (macAddress, ipAddress, method) {
 function getTime () {
 	return "[" + ((new Date() - startTime)/1000).toFixed(1) + "] ";
 }
+
+var characterArray = {"4542":"A","4543":"B","4544":"C","4545":"D","4546":"E","4547":"F","4548":"G","4549":"H","454a":"I","454b":"J","454c":"K","454d":"L","454e":"M","454f":"N","4550":"O","4641":"P","4642":"Q","4643":"R","4644":"S","4645":"T","4646":"U","4647":"V","4648":"W","4649":"X","464a":"Y","464b":"Z","4441":"0","4442":"1","4443":"2","4444":"3","4445":"4","4446":"5","4447":"6","4448":"7","4449":"8","444a":"9","4341":" ","4342":"!","4343":"\"","4344":"#","4345":"$","4346":"%","4347":"&","4348":"'","4349":"(","434a":")","434b":"*","434c":"+","434d":",","434e":"-","434f":".","444e":"=","444b":":","444c":";","4541":"@","464f":"^","4650":"_","484c":"{","484e":"}","484f":"~"};
